06cf350a235fe50ea59019be67af807fcbfaa4b9
bilalsamisaylani <101085324+bilalsami00@users.noreply.github.com>
2023-03-28 10:31:11 +0500

    conditonal rendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // conditional rendering
+
+  const [condition, setCondition] = useState(false)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        {
+          condition ? <img 
+          width={100} src='https://thumbs.dreamstime.com/z/light-blub-bulb-white-background-78995866.jpg'/> 
+          : <img width={100} 
+          src='https://www.shutterstock.com/image-vector/light-blub-vector-electric-inside-260nw-1751062736.jpg'/>
+        }
+
+        <button onClick={()=>{
+          setCondition(!condition)
+        }}>switch</button>
+
       </header>
     </div>
+
   );
 }
 
